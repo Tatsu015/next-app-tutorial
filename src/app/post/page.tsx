ef16ea9710d9d936c880d41aa4dd9b3f0d1d4c96
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-08-21 22:42:35 +0900

    add page

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,7 +1,7 @@
-export default function TopPage() {
+export default function PostPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Top page
+      post page
     </main>
   );
 }
