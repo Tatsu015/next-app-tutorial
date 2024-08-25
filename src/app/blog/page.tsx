c490ac15cc9c86ceae93e6c0a152861151f4fd01
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-08-25 10:06:10 +0900

    change name

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,7 @@
 export default function BlogPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      post page
+      Blog page
     </main>
   );
 }
